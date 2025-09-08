f5e8ed8738b768e369435ba54c5cb4bfbeb5d6c6
devchampps <abdullah.shafiq@live.com>
2025-09-08 20:09:18 +1000

    enable logging

--- .github/actions/my-cli-action/index.js
+++ .github/actions/my-cli-action/index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
 const { execSync } = require('child_process');
+const path = require('path');
+
 
 async function run() {
   try {
@@ -9,9 +11,10 @@ async function run() {
 
     //execSync(`curl -fsSL https://cli.example.com/install.sh | sh`, { stdio: 'inherit' });
 
-    //aws-cli/2.8.9
+    //aws-cli/2.8.9 -path.join(__dirname, 'awscli-bundle.zip');
     execSync(`curl "https://s3.amazonaws.com/aws-cli/awscli-bundle-2.8.9.zip" -o "awscli-bundle.zip" | sh`, { stdio: 'inherit' });
     execSync(`unzip "awscli-bundle.zip" . | sh`, { stdio: 'inherit' });
+    //const zipFile = path.join(__dirname, "awscli-bundle.zip")
     execSync(`sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws | sh`, { stdio: 'inherit' });
 
     console.log("MyCLI installed successfully.");
